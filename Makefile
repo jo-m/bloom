f9eb9bce9b5dbf6180ba1daf193e33d7f904971a
Jonathan Mueller <j.mueller@apoveda.ch>
2013-11-05 09:19:49 +0100

    renamed main to bloom_test

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 all: bloom_test
 
-bloom_test: main.o MurmurHash3.o bloom.o
-	gcc main.o MurmurHash3.o bloom.o -o bloom_test
+bloom_test: bloom_test.o MurmurHash3.o bloom.o
+	gcc bloom_test.o MurmurHash3.o bloom.o -o bloom_test
 
-main.o: main.c
-	gcc -c main.c
+bloom_test.o: bloom_test.c
+	gcc -c bloom_test.c
 
 MurmurHash3.o: MurmurHash3.c
 	gcc -c MurmurHash3.c
--- bloom_test.c
+++ bloom_test.c
@@ -9,14 +9,20 @@ int main ()
   bloom_data b;
   char *str1 = "This is a test string.";
   char *str2 = "This is another test string.";
-  bloom_init(&b, 32);
+  bloom_init(&b, BLOOM_MIN_LEN);
   bloom_insert(&b, str1, strlen(str1));
+  bloom_insert(&b, "123323123123", 10);
+  bloom_insert(&b, "123dsf23123123", 10);
+  bloom_insert(&b, "12334123123123", 10);
 
   if(bloom_lookup(&b, str1, strlen(str1)))
     printf("str1 found!\n");
 
   if(!bloom_lookup(&b, str2, strlen(str1)))
     printf("str2 not found!\n");
+
+  printf("%f\n", bloom_false_pos_prob(&b));
+  printf("%llu\n", bloom_num_bits_set(&b));
 
   return EXIT_SUCCESS;
 }
